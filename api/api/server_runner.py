568a00b4e7a3e05f5a43d56cbb6109d289f6eb6c
George Grainger <random.man552@protonmail.com>
2021-09-05 12:41:32 +0100

    Running servers now always close correctly
    A new process group is assigned to each server run, and the whole group is killed when stop_server is called.

--- api/api/server_runner.py
+++ api/api/server_runner.py
@@ -1,7 +1,9 @@
 import shlex
 import time
+import os
+import signal
 
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 
 from .models import GameServer
 from .socketIO import socketIO
@@ -65,6 +67,7 @@ def __create_process(working_directory: str, command: str) -> Popen:
         stdin=PIPE,
         stdout=PIPE,
         stderr=STDOUT,
+        start_new_session=True,
         cwd=working_directory
     )
 
@@ -74,11 +77,6 @@ def __start_watcher_threads(server_id: str) -> None:
     Helper function to start threads to watch the server process with the given id.
     :param server_id: The id of the server to watch.
     """
-    # out_watcher = threading.Thread(name=f"{server_id} stdout watcher", target=__read_stdout, args=(server_id,))
-    # poll_watcher = threading.Thread(name=f"{server_id} process watcher", target=__poll_process, args=(server_id,))
-    #
-    # out_watcher.start()
-    # poll_watcher.start()
 
     socketIO.start_background_task(__read_stdout, server_id)
     socketIO.start_background_task(__poll_process, server_id)
@@ -133,8 +131,22 @@ def stop_server(server: GameServer) -> bool:
     """
     server_id = str(server.id)
     if __running.get(server_id):
-        process = __running.pop(server_id)
-        process.terminate()
+        process: Popen = __running.pop(server_id)
+        pgid = os.getpgid(process.pid)
+
+        # Attempt to kill the process with SIGINT, if this fails try SIGKILL
+        try:
+            os.killpg(pgid, signal.SIGINT)
+            process.wait(5)
+        except TimeoutExpired:
+            # If SIGKILL fails, try SIGTERM
+            try:
+                os.killpg(pgid, signal.SIGKILL)
+                process.wait(5)
+            except TimeoutExpired:
+                # Finally try SIGTERM
+                os.killpg(pgid, signal.SIGTERM)
+                process.wait()
         return True
     return False
 
