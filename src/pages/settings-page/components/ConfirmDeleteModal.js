e79fbae92d8a026d75786e3384902720143800d0
George Grainger <random.man552@protonmail.com>
2021-09-08 16:17:44 +0100

    Add basic user management
    Currently all users have permission to edit all other users, hence a basic permission system is needed.

---
+++ src/pages/settings-page/components/ConfirmDeleteModal.js
@@ -0,0 +1,32 @@
+import {Button, Modal} from "react-bootstrap";
+import PropTypes from "prop-types";
+
+
+export function ConfirmDeleteModal(props) {
+    return (
+        <Modal
+            show={props.show}
+            onHide={props.onHide}
+        >
+            <Modal.Header>
+                <Modal.Title>Confirm Deletion?</Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+                <Button variant="danger" onClick={props.onConfirm}>Confirm</Button>
+                <Button variant="secondary" onClick={props.onCancel}>Cancel</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+ConfirmDeleteModal.propTypes = {
+    show: PropTypes.bool,
+
+    onHide: PropTypes.func,
+    onConfirm: PropTypes.func,
+    onCancel: PropTypes.func
+}
+
+ConfirmDeleteModal.defaultProps = {
+    show: false
+}
