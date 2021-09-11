4cfccb18d13d9b26f32058e84e9c852e5e1dfc93
George Grainger <random.man552@protonmail.com>
2021-09-11 18:06:30 +0100

    Add check for development environment for SocketIO

--- api/api/socketIO.py
+++ api/api/socketIO.py
@@ -1,4 +1,12 @@
+import os
+
 from flask_socketio import SocketIO
 
 
-socketIO = SocketIO(None, cors_allowed_origins="*")
+async_mode = "gevent_uwsgi"
+if os.getenv("FLASK_ENV").lower() == "development":
+    print("Development mode specified...")
+    print("Using threading for SocketIO...")
+    async_mode = "threading"
+
+socketIO = SocketIO(None, cors_allowed_origins="*", async_mode=async_mode)
