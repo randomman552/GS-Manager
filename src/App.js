64aebafcc667eb8722cdf887f1cb4696e0c078ea
George Grainger <random.man552@protonmail.com>
2021-10-04 13:30:34 +0100

    Improve login process
    Now only submits one login request, instead of a second request to get user details.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import { LoginPage } from "./pages/login-page/LoginPage";
 import { ServersPage } from "./pages/servers-page/ServersPage";
 import { SettingsPage } from "./pages/settings-page/SettingsPage";
-import { Modal, Button } from "react-bootstrap";
 import './App.css';
 import api from "./api/api";
 import MessageDisplay from "./pages/components/MessageDisplay";
@@ -13,7 +12,11 @@ import {SystemPage} from "./pages/system-page/SystemPage";
 export class App extends React.Component {
     constructor(props) {
         super(props);
-        // Attempt login from cookie on load
+        this.state = {
+            user: null
+        };
+
+        // Attempt login using old session
         api.auth.getCurrentUser().then(json => {
             if (json.success) {
                 this.setState({
@@ -23,14 +26,6 @@ export class App extends React.Component {
                 this.logout();
             }
         });
-
-        this.state = {
-            user: null,
-            modal: {
-                "title": null,
-                "message": null
-            }
-        };
     }
 
 
@@ -45,18 +40,12 @@ export class App extends React.Component {
         };
 
         api.auth.login(auth)
-            .then(() => {
-                api.auth.getCurrentUser()
-                    .then(json => {
-                    if (!json.error) {
-                        this.setState({
-                            user: json.data
-                        });
-                        api.auth.get().then();
-                    } else if (json.code === 401) {
-                        this.showModal("Login Failed", "Incorrect username or password...");
-                    }
-                });
+            .then(json => {
+                if (json.success) {
+                    this.setState({
+                        user: json.data
+                    });
+                }
             });
     }
 
@@ -64,10 +53,12 @@ export class App extends React.Component {
      * Destroy current session.
      */
     logout() {
-        api.auth.logout().then(() => {
-            this.setState({
-                "user": null
-            });
+        api.auth.logout().then(json => {
+            if (json.success) {
+                this.setState({
+                    "user": null
+                });
+            }
         });
     }
 
@@ -83,75 +74,14 @@ export class App extends React.Component {
     }
 
 
-    /**
-     * Method to show the global modal in the application.
-     * @param title The title to display
-     * @param message The message to display
-     */
-    showModal(title, message) {
-        this.setState({
-            "modal": {
-                "show": true,
-                "title": title,
-                "message": message
-            }
-        });
-    }
-
-    hideModal() {
-        this.setState({
-            "modal": {
-                "show": false,
-                "title": this.state.modal.title,
-                "message": this.state.modal.message
-            }
-        });
-    }
-
-    renderModal() {
-        const show = this.state.modal.show;
-        const title = this.state.modal.title;
-        const message = this.state.modal.message;
-
-        const onHide = () => { this.hideModal() };
-
-        return (
-            <Modal
-                show={show}
-                onHide={onHide}
-                keyboard={true}
-            >
-              <Modal.Header closeButton>
-                <Modal.Title>{title}</Modal.Title>
-              </Modal.Header>
-
-              <Modal.Body>
-                <p>
-                    {message}
-                </p>
-              </Modal.Body>
-
-              <Modal.Footer>
-                <Button variant="primary" onClick={onHide}>Close</Button>
-              </Modal.Footer>
-            </Modal>
-        )
-}
-
-
     render() {
-        const modal = this.renderModal();
         if (!this.state.user) {
             return (
                 <div id="app">
                     <MessageDisplay/>
-                    {modal}
-                    <Switch>
-                        <Route exact path="/login" render={ () => {
-                            return <LoginPage onLogin={ (u, p) => this.login(u, p) }/>
-                        }} />
-                        <Redirect to="/login" />
-                    </Switch>
+                    <LoginPage
+                        onLogin={ (u, p) => this.login(u, p) }
+                    />
                 </div>
             );
         }
