f93610d9d99dc61c17a6fa0ae702b1d3c95623d1
George Grainger <random.man552@protonmail.com>
2021-10-20 17:54:54 +0100

    Prevent back-end from starting servers with an empty command

--- api/api/server_runner.py
+++ api/api/server_runner.py
@@ -110,6 +110,8 @@ def update_server(server: GameServer) -> bool:
     """
     server_id = str(server.id)
     if not __running.get(server_id) or not server.is_running:
+        if not server.update_cmd:
+            return False
         process = __create_process(server.working_directory, server.update_cmd)
         __running[server_id] = process
 
