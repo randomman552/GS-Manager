d0d9340b6d3f3c226802512ec24acca03463c45e
George Grainger <random.man552@protonmail.com>
2021-10-26 16:52:54 +0100

    Add collapse region to navigation

--- src/pages/components/Navigation.js
+++ src/pages/components/Navigation.js
@@ -9,37 +9,45 @@ export function Navigation(props) {
     const onLogout = props.onLogout;
 
     return (
-        <header>
-            <Navbar bg="white">
-                <Navbar.Brand>
-                    GS-Manager
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="main-navbar" />
-                <Navbar.Collapse id="main-navbar">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} className="text-capitalize" to="/servers">
-                            Servers
-                        </Nav.Link>
-                        <Nav.Link as={Link} className="text-capitalize" to="/system">
-                            System
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav className="me-auto">
-                        <NavDropdown id="user-info" title={loggedInMessage} alignRight={true}>
-                            <NavDropdown.Item onClick={onLogout}>
-                                Logout
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item as={Link} to="/settings">
-                                Settings
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
+        <Navbar
+            bg="white"
+            collapseOnSelect={true}
+            expand="sm"
+            variant="light"
+            fixed={"top"}
+        >
+            <Navbar.Brand href="/">
+                GS-Manager
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="main-navbar"/>
+            <Navbar.Collapse id="main-navbar">
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} className="text-capitalize" to="/servers">
+                        Servers
+                    </Nav.Link>
+                    <Nav.Link as={Link} className="text-capitalize" to="/system">
+                        System
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav className="me-auto">
+                    <NavDropdown
+                        id="user-info"
+                        title={loggedInMessage}
+                        alignRight={true}
+                    >
+                        <NavDropdown.Item onClick={onLogout}>
+                            Logout
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item as={Link} to="/settings">
+                            Settings
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
